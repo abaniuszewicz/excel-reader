fa14812bc695ce3d246c5b68a4aa96dafb89b76a
abaniuszewicz <abaniuszewicz@gmail.com>
2020-12-19 14:13:00 +0100

    introducing ILogger & configuring serilog as demo logger implementation (#2)

--- ExcelReader.Demo/Program.cs
+++ ExcelReader.Demo/Program.cs
@@ -2,8 +2,13 @@
 using ExcelReader.Demo.CustomImplementations;
 using ExcelReader.Extensions.Autofac;
 using ExcelReader.Readers;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
 using System.Data;
 using System.IO;
+
+using MicrosoftLogging = Microsoft.Extensions.Logging;
 
 namespace ExcelReader.Demo
 {
@@ -14,13 +19,25 @@ namespace ExcelReader.Demo
             FileInfo file = new FileInfo("SampleFiles/MultipleSheets.xlsx");
 
             ContainerBuilder containerBuilder = new ContainerBuilder();
-            containerBuilder.RegisterCustomExcelReader()
-                .UseReader<CustomReader>()
-                .Configure();
+            containerBuilder.RegisterExcelReaderWithDefaults()
+                .RegisterInstance(ConfigureLogger()).As<MicrosoftLogging.ILogger>().SingleInstance();
             IContainer container = containerBuilder.Build();
 
             IReader reader = container.Resolve<IReader>();
             reader.Read(file);
+        }
+
+        private static MicrosoftLogging.ILogger ConfigureLogger()
+        {
+            Serilog.ILogger logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss:fff} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            LoggerFactory factory = new LoggerFactory();
+            factory.AddSerilog(logger);
+            return factory.CreateLogger<MicrosoftLogging.ILogger>();
         }
     }
 }
--- ExcelReader/Readers/Reader.cs
+++ ExcelReader/Readers/Reader.cs
@@ -1,5 +1,6 @@
 ﻿using ExcelReader.Sets;
 using ExcelReader.Utilities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 
@@ -7,17 +8,32 @@ namespace ExcelReader.Readers
 {
     public sealed class Reader : IReader
     {
+        private readonly ILogger _logger;
+
+        public Reader(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public ISet Read(FileInfo file)
         {
-            if (file == null)
+            if (file is null)
+            {
+                _logger.LogError("File {0} is null", file.Name);
                 throw new ArgumentNullException(nameof(file), "File cannot be null");
+            }
             if (!file.Exists)
+            {
+                _logger.LogError("File {0} does not exist", file.Name);
                 throw new ArgumentException($"File {file.FullName} does not exist", nameof(file));
+            }
 
             DirectoryInfo extractionDirectory = GetExtractionDirectory();
             try
             {
+                _logger.LogDebug("Extracting {0} to {1}...", file.Name, extractionDirectory.FullName);
                 Unzipper.Unzip(file, extractionDirectory);
+                _logger.LogDebug("Extraction complete");
             }
             finally
             {
